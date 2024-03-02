2869705b4047c6fe6a97b2023fb5e143c838260f
E302 sede B Tecnologico Comfenalco <E302@Academico.local>
2024-03-02 11:54:22 -0500

    confirmar

---
+++ TALLER-POO/src/api/vehiculo/model/Vehiculo.java
@@ -0,0 +1,9 @@
+
+package api.vehiculo.model;
+
+public interface Vehiculo {
+    public void acelerar();
+    public void frenar();
+    public void estacionar();
+    public void direccionando();
+}
